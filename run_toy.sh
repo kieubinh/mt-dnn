0a455f656585fcd8d394e27468e3c33a4910465a
Thanh Binh Kieu <13616267@adsroot.uts.edu.au>
2019-10-04 18:09:26 +1000

    update gitignore + bash run

--- run_toy.sh
+++ run_toy.sh
@@ -5,5 +5,7 @@
 # Note that this is a toy setting and please refer our paper for detailed hyper-parameters.
 ############################### 
 
+#python prepro_std.py
+#experiments/glue/prepro.sh
 python prepro.py
 python train.py
--- scripts/run_mt_dnn.sh
+++ scripts/run_mt_dnn.sh
@@ -12,7 +12,7 @@ tstr=$(date +"%FT%H%M")
 
 train_datasets="mnli,rte,qqp,qnli,mrpc,sst,cola,stsb"
 test_datasets="mnli_matched,mnli_mismatched,rte"
-MODEL_ROOT="checkpoints"
+MODEL_ROOT="reproduce_mt_dnn"
 BERT_PATH="../mt_dnn_models/bert_model_large_uncased.pt"
 DATA_DIR="../data/canonical_data/mt_dnn_uncased_lower"
 
@@ -22,6 +22,6 @@ grad_clipping=0
 global_grad_clipping=1
 lr="5e-5"
 
-model_dir="checkpoints/${prefix}_${optim}_answer_opt${answer_opt}_gc${grad_clipping}_ggc${global_grad_clipping}_${tstr}"
+model_dir="${MODEL_ROOT}/${prefix}_${optim}_answer_opt${answer_opt}_gc${grad_clipping}_ggc${global_grad_clipping}_${tstr}"
 log_file="${model_dir}/log.log"
 python ../train.py --data_dir ${DATA_DIR} --init_checkpoint ${BERT_PATH} --batch_size ${BATCH_SIZE} --output_dir ${model_dir} --log_file ${log_file} --answer_opt ${answer_opt} --optimizer ${optim} --train_datasets ${train_datasets} --test_datasets ${test_datasets} --grad_clipping ${grad_clipping} --global_grad_clipping ${global_grad_clipping} --learning_rate ${lr} --multi_gpu_on
